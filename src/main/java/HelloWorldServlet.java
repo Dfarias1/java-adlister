93526a6eceb5145b28397436b04a69d5485f004f
Daniel <daniel.h.farias2@gmail.com>
2023-05-16 14:40:20 -0500

    HelloWorldServlet

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,31 +1,22 @@
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.lang.reflect.Method;
-import java.text.MessageFormat;
-import java.util.Enumeration;
-import java.util.ResourceBundle;
-import javax.servlet.GenericServlet;
-import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
-        out.println("<h1>Hello, World</h1>");
 
         String name = req.getParameter("name");
 
-        if (name == null){
+        if (name == null) {
             name = "World";
         }
+
         out.println("<h1>Hello, " + name + "!</h1>");
     }
 }
